3f214151d191bfbd370524916a55cb169b5f72d7
lakesare <eugekrns@yandex.ru>
2014-12-02 12:08:13 +0200

    npmas

---
+++ lib/processors/npmas.rb
@@ -0,0 +1,129 @@
+class Npmas
+  def initialize
+    @host = 'http://www.npmas.ru'
+    @list_link = 'http://www.npmas.ru/members.php'
+    @data_link_template = ''
+    @required_fields = [
+      :inn,
+      :name,
+      :short_name,
+      :city,
+      :status,
+      :resolution_date,
+      :legal_address,
+      :certificate_number,
+      :ogrn
+    ]
+    @data = []
+  end
+
+  def perform
+    collect_links # Сбор ссылок
+    iterate # Переход и сбор информации #time
+    @data
+  end
+
+  private
+
+  def collect_links
+    doc = Nokogiri::HTML(open(@list_link))
+    pages = [@list_link] 
+    doc.css('table+p a')[5..7].each do |link|
+      pages.push "#{@host}/#{link['href']}"
+    end
+
+    @links = [] 
+    pages.each do |page|
+      doc = Nokogiri::HTML(open(page))
+      doc.css('table.members a').each do |link|
+        @links.push "#{@host}/#{link['href']}"
+      end
+    end
+  end
+
+  def iterate
+    @links.each do |link|
+      puts "openinig #{link}\n"
+      begin
+        Capybara.visit(link)
+      rescue => detail
+        tell detail
+        puts 'next link'
+        next #if link is inaccessible
+      end
+
+      tmp = Hash.new
+      @required_fields.each do |m|
+          value = self.send(m) #for status symbols
+          value = value.strip if value.is_a? String
+          tmp.merge! m => value
+      end
+      @data << tmp #@data = [tmp, {@required_fields[0] => 'value'}]
+    end
+    @data
+  end
+
+  #### Fields methods ####
+
+  ## Required fields ##
+  def inn
+    raw = Capybara.first('td strong', text: 'ИНН').first(:xpath, '../..').first('td:nth-child(2)').text
+  end
+
+  def short_name
+    '-'
+  end
+
+  def name
+    raw = Capybara.first('td strong', text: 'Название').first(:xpath, '../..').first('td:nth-child(2)').text
+  end
+
+  def city
+    raw = Capybara.first('td strong', text: 'Место нахождения').first(:xpath, '../..').first('td:nth-child(2)').text
+
+    test1 = raw.match /\b((пос|гор|пгт|рп)\. [А-Яа-я\- ]+)\b/
+    test2 = raw.match /\b([гсдп]\. ?[А-Яа-я\- ]+)\b/
+    test3 = raw.match /\b((р.п.|рабочий поселок) [А-Яа-я\- ]+)\b/
+
+    if test1
+      test1[1]  
+    elsif test2
+      test2[1]
+    elsif test3
+      test3[1]
+    else
+      Capybara.first('td strong', text: 'Регион').first(:xpath, '../..').first('td:nth-child(2)').text
+    end
+  end
+
+  def status
+    raw = Capybara.first('td strong', text: 'Статус допуска').first(:xpath, '../..').first('td:nth-child(2)').text
+    if raw.include? 'действующий'
+      :w 
+    elsif raw.include? 'исключена'
+      :e
+    else
+      '-'
+    end
+  end
+
+  def resolution_date
+    raw = Capybara.first('td strong', text: 'Дата регистрации в реестре в соответствии с протоколом').first(:xpath, '../..').first('td:nth-child(2)').text
+    resolution_date = raw.include?(' от ') ? raw.split(' от ')[1] : raw
+  end
+
+  def legal_address
+    raw = Capybara.first('td strong', text: 'Место нахождения').first(:xpath, '../..').first('td:nth-child(2)').text
+  end
+
+  def certificate_number
+    raw = Capybara.first('td strong', text: 'Номер свидетельства').first(:xpath, '../..').first('td:nth-child(2)').text
+  end
+
+  def ogrn
+    raw = Capybara.first('td strong', text: 'ОГРН').first(:xpath, '../..').first('td:nth-child(2)').text
+  end
+end
+
+
+
--- lib/processors/odinsro_stroy.rb
+++ lib/processors/odinsro_stroy.rb
@@ -1,18 +1,18 @@
-class Odinso_stroy
+class OdinsroStroy
   def initialize
     @host = 'http://odinsro-stroy.ru'
-    @list_link = 'http://odinsro-stroy.ru/reestr/'
+    @list_link = 'http://odinsro-stroy.ru/reestr'
     @data_link_template = ''
     @required_fields = [
-        :inn,
-        :name,
-        :short_name,
-        :city,
-        :status,
-        :resolution_date,
-        :legal_address,
-        :certificate_number,
-        :ogrn
+      :inn,
+      :name,
+      :short_name,
+      :city,
+      :status,
+      :resolution_date,
+      :legal_address,
+      :certificate_number,
+      :ogrn
     ]
     @data = []
   end
@@ -41,19 +41,21 @@ class Odinso_stroy
         @doc = Nokogiri::HTML(open(URI.encode(link)))
       rescue
         puts 'next link'
-        next
+        next #if link is inaccessible
       end
 
       tmp = Hash.new
       @required_fields.each do |m|
         begin
-          tmp.merge!(m => self.send(m))
+          value = self.send(m) #for status symbols
+          value = value.strip if value.is_a? String
+          tmp.merge! m => value
         rescue
-          tmp.merge!(m => '-')
-        end
+          tmp.merge!(m => '-') #if there was an error in data retrieval, pretend it's -
+        end 
       end
 
-      @data << tmp if tmp[:status] #@data = [tmp, {@required_fields[0] => 'value'}]
+      @data << tmp #@data = [tmp, {@required_fields[0] => 'value'}]
     end
   end
 
@@ -61,71 +63,61 @@ class Odinso_stroy
 
   ## Required fields ##
   def inn
-    slice = @doc.css('li:not(:first-child) td.t5').text.split('/')
-    slice[0].strip
+    raw = @doc.css('li:not(:first-child) td.t5').text
+    raw.split('/')[0]
   end
 
   def short_name
-    slice = @doc.css('li:not(:first-child) td.t3').text.split('/ ')
-    slice[1] ? slice[1].strip : '-'
+    raw = @doc.css('li:not(:first-child) td.t3').text
+    raw.split('/')[1]
   end
 
   def name
-    slice = @doc.css('li:not(:first-child) td.t3').text
-    slice.split('/ ')[0].strip
+    raw = @doc.css('li:not(:first-child) td.t3').text
+    raw.split('/')[0]
   end
 
   def city
-    slice = @doc.css('li:not(:first-child) td.t4').text
+    raw = @doc.css('li:not(:first-child) td.t4').text
+    test1 = raw[/[пгс][.]\s?[А-Яа-я\- ]+/]
+    test2 = raw[/,\s?[А-Яа-я\- ]+\s[гп].?\w?,/]
+    test4 = raw[/,\s?[А-Яа-я\- ]+/]
+    city =  
+      if test1
+        test1.to_s
+      elsif test2
+        test2.to_s
+      elsif test4
+        test3.to_s
+      end
 
-    if slice.include? 'г. '
-      slice = 'г. ' + slice.split('г. ')[1].split(' ')[0]
-    elsif slice.include? 'п. '
-      slice = 'п. ' + slice.split('п. ')[1].split(' ')[0]
-    elsif slice.include? ' г,'
-      slice = 'г. ' + slice.split(' г,')[0].strip.split(' ').last
-    elsif slice.include? ' п,'
-      slice = 'п. ' + slice.split(' п,')[0].strip.split(' ').last
-    else
-      slice = '-'
-    end
-    slice.slice!(',') if slice.include? ','
-    slice.strip
+    city.gsub(',', '')
   end
 
   def status
-    slice = @doc.css('li:not(:first-child) td.t7').text    
-    if slice.include? '----------'
-      :w
-    elsif slice.include? 'Исключен' or slice.include? 'Выбыл'
-      false
-    else
-      '-'
-    end
+    raw = @doc.css('li:not(:first-child) td.t7').text    
+    return :w if raw.include? '-------'
+    return :e if (raw.include? 'Исключен') || (raw.include? 'Выбыл')
+    '-'
   end
 
   def resolution_date
-    slice = @doc.css('tr:nth-child(8) span.font5').text
-    slice = @doc.css('tr:nth-child(8) p:first-child').text if slice == ""
-    slice.split(' от ')[1].strip
+    raw = @doc.css('tr:nth-child(8) td:nth-child(2)').text
+    raw[/\d{2}\.\d{2}\.\d{4}/] #gets first date from td
   end
 
   def legal_address
-    slice = @doc.css('li:not(:first-child) td.t4').text
+    raw = @doc.css('li:not(:first-child) td.t4').text
   end
 
   def certificate_number
-    slice = @doc.css('tr:nth-child(8) span.font5').text
-    if slice.include? 'свидетельство'
-      slice.split('свидетельство')[1].split('от')[0].strip 
-    else
-      slice.split('от')[0].strip
-    end
+    raw = @doc.css('tr:nth-child(8) td:nth-child(2)').text
+    raw[/№\s?[\w\.\-А-Яа-я]+/]
   end
 
   def ogrn
-    slice = @doc.css('li:not(:first-child) td.t5').text.split('/')
-    slice[1].strip
+    raw = @doc.css('li:not(:first-child) td.t5').text
+    raw.split('/')[1]
   end
 end
 
