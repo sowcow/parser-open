9cc5c8b6cc06f0018abded4b3bce3329be78b605
lakesare <eugekrns@yandex.ru>
2014-12-04 05:59:37 +0200

    SroDst

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,13 +1,13 @@
 require 'capybara/poltergeist'
 
-Capybara.register_driver :poltergeist do |app|
+Capybara.register_driver :selenium do |app|
   Capybara::Poltergeist::Driver.new(
     app,
     js_errors: false,
-    phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'],
+   # phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'],
     timeout: 90)
 end
 
-Capybara.default_driver = :poltergeist
-Capybara.javascript_driver = :poltergeist
+Capybara.default_driver = :selenium
+Capybara.javascript_driver = :selenium
 Capybara.run_server = false
